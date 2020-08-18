8498aa8db4e168e49a1490a408e25d2fb319717f
Bryan Rodriguez <bsteven1993@gmail.com>
2020-08-18 18:39:04 -0500

    view profile friend

--- lib/profile/profile_2.dart
+++ lib/profile/profile_2.dart
@@ -40,7 +40,7 @@ class Profile2 extends StatelessWidget {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              height: 400,
+              height: 500,
               padding: EdgeInsets.symmetric(horizontal: 16),
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -216,7 +216,79 @@ class Profile2 extends StatelessWidget {
 
                       ],
                     ),
-                  )
+                  ),
+
+                  Divider(
+                    color: Colors.grey[400],
+                  ),
+
+                  Text(
+                    'Friends',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+
+                  Container(
+                    height: 70,
+                    child: ListView.builder(
+                      padding: EdgeInsets.all(8),
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 9,
+                      itemBuilder: (_, index){
+                        return Container(
+                          height: 60,
+                          width: 60,
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/friends_${index.toString()}.jpg'),
+                              fit: BoxFit.cover
+                            )
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+
+                  SizedBox(height: 10,),
+
+                  Text(
+                    'Photos',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+
+                  Container(
+                    height: 160,
+                    child: ListView.builder(
+                      padding: EdgeInsets.all(8),
+                      physics: BouncingScrollPhysics(),
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 9,
+                      itemBuilder: (_, index){
+                        return Container(
+                          height: 160,
+                          width: 110,
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/photo_${index.toString()}.jpg'),
+                              fit: BoxFit.cover
+                            )
+                          ),
+                        );
+                      },
+                    ),
+                  ),
 
                 ],
               ),
